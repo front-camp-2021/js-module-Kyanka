69b47c91c27de58345abe3b1f32c324d4e0e4aca
Kyanka <mozgoprod@gmail.com>
2021-10-04 14:51:34 +0300

    module-1: cutStrings done

--- src/module-1/cutStrings/index.js
+++ src/module-1/cutStrings/index.js
@@ -1,1 +1,6 @@
-export const cutStrings = (arr = []) => {};
+export const cutStrings = (arr = []) => {
+  return arr.map((str) => str.slice(0, arr.reduce((a,b) => a.length <= b.length ? a : b).length));
+};
+
+
+
