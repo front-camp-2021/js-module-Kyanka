0c4c04cfddfa94aaa675caa6027f2495b0bd0598
Kyanka <mozgoprod@gmail.com>
2021-10-04 16:08:32 +0300

    module-1: debounce done

--- src/module-1/debounce/index.js
+++ src/module-1/debounce/index.js
@@ -1,5 +1,12 @@
 export const debounce = (fn, delay = 0) => {
-
+  let timeout = false;
+  return function() {
+    if (timeout) return;
+    fn.apply(this, arguments);
+    timeout = true;
+    setTimeout(() => timeout = false, delay);
+  };
 }
+
 
 
