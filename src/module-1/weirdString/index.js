ca987b095667802cd250702dacd73deff2ac12b7
Kyanka <mozgoprod@gmail.com>
2021-10-04 17:18:27 +0300

    module-1: weirdString done

--- src/module-1/weirdString/index.js
+++ src/module-1/weirdString/index.js
@@ -1,1 +1,4 @@
-export const weirdString = (str = "") => {};
+export const weirdString = (str = "") => {
+ return str.split(" ").map(str => str.length > 0 ? str.slice(0,-1).toUpperCase() + str[str.length-1].toLowerCase() : "").join(" ")
+};
+
